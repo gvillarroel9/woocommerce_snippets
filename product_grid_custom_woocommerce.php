de999886b8ac8a16796bbecb90f4c55d1d65b65e
Geraldo Villarroel <gvillarroel9@gmail.com>
2020-06-20 18:05:20 -0400

    changes  args in product_grid_woocoommerce

--- product_grid_custom_woocommerce.php
+++ product_grid_custom_woocommerce.php
@@ -1,39 +1,52 @@
-function property_custom_grid($atts = [], $content = null, $tag = ''){
-    $atts = array_change_key_case((array)$atts, CASE_LOWER); 
-    $wporg_atts = shortcode_atts(
-		[
-			'category' => '',
-			'qty' => '',
-		], 
-		$atts, 
-		$tag);
-		$products = wc_get_products(array(
-    		'category' => array($atts['category']),
-		));
+//[property_grid category="santiago" qty="4"]
 
+	function property_custom_grid($atts = [], $content = null, $tag = ''){
+    	$atts = array_change_key_case((array)$atts, CASE_LOWER); 
+    	$wporg_atts = shortcode_atts(
+			[
+				'category' => '',
+				'qty' => '',
+			], 
+			$atts, 
+			$tag);
+		
 		$args = array(
 			'post_type'      => 'product',
-			'posts_per_page' => 10,
-			//'product_cat'    => 'hoodies'
+			'posts_per_page' => 10
 		);
-
-		$grid_cards= '<div class="grid-card-products">';
+		
+		if(!empty($wporg_atts['category'])){
+			$args['product_cat'] = $wporg_atts['category'];
+		}
+		
 		$products = new WP_Query( $args );
-			foreach ($products->posts as $product){
-				$grid_cards .='<div class="card"><div class="card-header">';
-				$current_product = wc_get_product($product->ID);
-				$grid_cards .= '<div>'.$current_product->get_name().'</div>';
-				$grid_cards .= '<div class="card-header-icon"><i class="fa fa-map-marker"></i></div></div>';
-				$grid_cards .= '<div class="card-img" style="background-image: url('
-							.wp_get_attachment_image_url($current_product->get_image_id(),'full').
-							');">';
-				$grid_cards .= '<div class="card-img-icon"><img src="https://www.tearriendo.cl/wp-content/uploads/2020/06/icono-cama-04.png">';
-				$grid_cards .= '<h1>'.get_post_meta( $product->ID, '_product_attributes')[0]['dormitorio']['value'].'H</h1></div>';
-				$grid_cards .= '<div class="card-img-icon"><img src="http://www.tearriendo.cl/wp-content/uploads/2020/06/icono-ducha-05.png">';
-				$grid_cards .= '<h1>'.get_post_meta( $product->ID, '_product_attributes')[0]['bano']['value'].'B</h1></div></div>';
-				$grid_cards .= '<a class="card-footer" href='.get_permalink( $product->ID ).'>';
-                $grid_cards .='<div>Detalles de la propiedad</div><div class="card-footer-icon"><i class="fa fa-search"></i></div></a></div>';
-			}
+		
+		$quantity = count($products->posts);
+		
+		if($quantity<0){
+			return 'No hay productos para mostrar';
+		}
+		
+		if(!empty($wporg_atts['qty'])&&$wporg_atts['qty']<$quantity){
+			$quantity =$wporg_atts['qty'];
+		}
+		
+		$grid_cards= '<div class="grid-card-products">';
+		for($i=0; $i<$quantity; $i++){
+			$grid_cards .='<div class="card"><div class="card-header">';
+			$current_product = wc_get_product($products->posts[$i]->ID);
+			$grid_cards .= '<div>'.$current_product->get_name().'</div>';
+			$grid_cards .= '<div class="card-header-icon"><i class="fa fa-map-marker"></i></div></div>';
+			$grid_cards .= '<div class="card-img" style="background-image: url('
+				.wp_get_attachment_image_url($current_product->get_image_id(),'full').
+				');">';
+			$grid_cards .= '<div class="card-img-icon"><img src="https://www.tearriendo.cl/wp-content/uploads/2020/06/icono-cama-04.png">';
+			$grid_cards .= '<h1>'.get_post_meta( $products->posts[$i]->ID, '_product_attributes')[0]['dormitorio']['value'].'H</h1></div>';
+			$grid_cards .= '<div class="card-img-icon"><img src="https://www.tearriendo.cl/wp-content/uploads/2020/06/icono-ducha-05.png">';
+			$grid_cards .= '<h1>'.get_post_meta( $products->posts[$i]->ID, '_product_attributes')[0]['bano']['value'].'B</h1></div></div>';
+			$grid_cards .= '<a class="card-footer" href='.get_permalink( $products->posts[$i]->ID ).'>';
+			$grid_cards .='<div>Detalles de la propiedad</div><div class="card-footer-icon"><i class="fa fa-search"></i></div></a></div>';
+		}
 		$grid_cards .= '</div>';
 		$grid_cards .= ' <style>
 
